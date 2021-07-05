7e5d9ae28a51a8115614b5d730e715885c8d4f70
Daniel Libanori <daniel.lb68@gmail.com>
2021-07-05 16:22:22 -0300

    Added some fields to the login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Paper } from '@material-ui/core';
 import { CenterContainer, TabLogin } from './login-styles';
+import { TextField, Button } from '@material-ui/core';
 
 const Login : React.FC = () => {
 
@@ -8,8 +9,10 @@ const Login : React.FC = () => {
         <CenterContainer>
             <Paper style={{width: "80vw", margin:30, height: "80vh", backgroundColor: "#FEFEFE"}}>
                 <div style={{display:"flex"}}><TabLogin/><TabLogin/></div>
-                
-                
+                <p>Digite seu login e senha</p>
+                <TextField id="outlined-basic" label="Email" variant="outlined"/>
+                <TextField id="outlined-basic" label="Senha" variant="outlined"/>
+                <Button variant="contained" color="primary">Entrar</Button>
             </Paper>
         </CenterContainer>
     )
